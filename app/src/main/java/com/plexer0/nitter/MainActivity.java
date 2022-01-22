d77d9b455399b5a41bca28e0bf2e94436a5316f0
Plexer <plexer0@protonmail.com>
2022-01-22 16:07:49 +1100

    Remove unneeded dependencies

--- app/src/main/java/com/plexer0/nitter/MainActivity.java
+++ app/src/main/java/com/plexer0/nitter/MainActivity.java
@@ -7,8 +7,6 @@ import android.os.Bundle;
 import android.view.View;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
-import android.widget.Button;
-import android.widget.EditText;
 
 public class MainActivity extends Activity {
 
